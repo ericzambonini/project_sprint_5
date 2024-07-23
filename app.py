81c2ecd3a5f31d8405a1e5a9292c5d9c46697916
Zambo <ericzambonini@gmail.com>
2024-07-23 14:30:28 +0200

    modificação com coreções de bugs

--- app.py
+++ app.py
@@ -1,35 +1,25 @@
 import streamlit as st
-import pandas as ps
-import plotly_express as px
+import pandas as pd
+import plotly.express as px
 
-st.header('Histograma de vendas de carros')
+# Cabeçalho da aplicação
+st.header('Visualizações de vendas de carros')
 
-car_data = pd.read_csv('vehicles.csv')  # lendo os dados
-hist_button = st.button('Criar histograma')  # criar um botão
+# Lendo os dados
+car_data = pd.read_csv('vehicles.csv')
 
-if hist_button:  # se o botão for clicado
-    # escrever uma mensagem
-    st.write(
-        'Criando um h')
+# Criar botões com chaves únicas
+hist_button = st.button('Criar histograma', key='hist_button')
 
-    # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
+scatter_button = st.button('Criar gráfico de dispersão', key='scatter_button')
 
-    # exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)
-
-    # ---------------------------------------------------
-
-# Criar botões
-hist_button = st.button('Criar histograma')
-scatter_button = st.button('Criar gráfico de dispersão')
-
-# Ação ao clicar no botão de histograma
+# Ação ao clicar no primeiro botão de histograma
 if hist_button:
     st.write(
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+
 
 # Ação ao clicar no botão de gráfico de dispersão
 if scatter_button:
@@ -39,11 +29,10 @@ if scatter_button:
                      title="Dispersão de Preço vs Odômetro")
     st.plotly_chart(fig, use_container_width=True)
 
-    # --------------------------------------------------
-
-    # Criar caixas de seleção
-hist_checkbox = st.checkbox('Criar histograma')
-scatter_checkbox = st.checkbox('Criar gráfico de dispersão')
+# Criar caixas de seleção com chaves únicas
+hist_checkbox = st.checkbox('Criar histograma', key='hist_checkbox')
+scatter_checkbox = st.checkbox(
+    'Criar gráfico de dispersão', key='scatter_checkbox')
 
 # Ação ao selecionar a caixa de seleção do histograma
 if hist_checkbox:
